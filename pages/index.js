3bcb333a20040c0ae4980c2a06c0044588c4f54c
Archit Gupta <gupta.archit01@gmail.com>
2023-09-24 23:36:38 +0530

    Added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,58 @@
-// import Head from "next/head";
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
+import { useEffect, useState } from 'react';
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
-  return <div className="text-red-600 p-10 ">Dev</div>;
+const prisma = new PrismaClient();
+
+export default  function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  // const data = prisma.user.create({
+  //   data: {
+  //     name: "John Doe"
+  //   }
+  // });
+
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const fetchedUsers = await prisma.user.findMany();
+        console.log(fetchedUsers);
+        setUsers(fetchedUsers);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.createdAt.toString()}</td>
+              <td>{user.updatedAt.toString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
